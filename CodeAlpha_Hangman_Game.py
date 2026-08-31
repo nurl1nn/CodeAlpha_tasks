e945ed8cf6cee15cc74d2af0798b6c22f3dfb598
Nurlan Inayatov <inayatovnurl1n@gmail.com>
2026-08-31 12:45:24 +0400

    Refactor Hangman game code structure

--- CodeAlpha_Hangman_Game.py
+++ CodeAlpha_Hangman_Game.py
@@ -1,4 +1,3 @@
-"""
 import random
 words = ["python", "matrix", "robot", "cloud", "linux"]
 limit=6
@@ -19,28 +18,3 @@ while limit>0:
              limit -= 1
              print(f"Try again \n{limit} Guess left") 
 print(f"You lost! The word was: {chosen_word}")   
-"""
-stock_prices = {
-    "AAPL": 180,
-    "TSLA": 250,
-    "GOOGL": 140,
-    "AMZN": 185,
-    "MSFT": 415,
-    "NVDA": 875,
-    "META": 510,
-    "NFLX": 635
-}
-total=0
-while input("Yeni sehm almaq isteyirsiniz? (Y/N): ").upper() == "Y":
-          sehm = input("Enter a name: ").upper()
-          if sehm not in stock_prices:
-               print("This stock is not available.")
-               continue
-          else:
-               times =int(input("How much do you want: "))
-               Umumi=stock_prices.get(sehm) * times
-               total += Umumi
-               with open("stoklar.txt","a") as file:
-                     file.write(f"Sehm: {sehm}, Times: {times}, Total: {Umumi}\n")
-with open("stoklar.txt", "r") as file:            
-     print(f"\nThank you for using our service! \nThis is your list: \n{file.read()} And your total is: {total}")
